4a2d27d3782e9309bc02cead3d95474230852aa2
Kilian Chung <kilian.chung@web.de>
2022-04-14 00:08:59 +0200

    cleaned up code

--- solver.c
+++ solver.c
@@ -16,7 +16,7 @@
 #define EMPTY_SQUARE 1
 #if __linux__
 #include <strings.h>
-#elif _WIN32
+#else
 int ffs(int i) {
   static const unsigned char table[] = {
     0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8
@@ -35,18 +35,15 @@ int ffs(int i) {
 const int grpdict[SQUARES_NUM][GROUPS_PER_SQUARE] = {
   0, 9, 18, 0, 9, 19, 0, 9, 20, 1, 9, 21, 1, 9, 22, 1, 9, 23, 2, 9, 24, 2, 9, 25, 2, 9, 26, 0, 10, 18, 0, 10, 19, 0, 10, 20, 1, 10, 21, 1, 10, 22, 1, 10, 23, 2, 10, 24, 2, 10, 25, 2, 10, 26, 0, 11, 18, 0, 11, 19, 0, 11, 20, 1, 11, 21, 1, 11, 22, 1, 11, 23, 2, 11, 24, 2, 11, 25, 2, 11, 26, 3, 12, 18, 3, 12, 19, 3, 12, 20, 4, 12, 21, 4, 12, 22, 4, 12, 23, 5, 12, 24, 5, 12, 25, 5, 12, 26, 3, 13, 18, 3, 13, 19, 3, 13, 20, 4, 13, 21, 4, 13, 22, 4, 13, 23, 5, 13, 24, 5, 13, 25, 5, 13, 26, 3, 14, 18, 3, 14, 19, 3, 14, 20, 4, 14, 21, 4, 14, 22, 4, 14, 23, 5, 14, 24, 5, 14, 25, 5, 14, 26, 6, 15, 18, 6, 15, 19, 6, 15, 20, 7, 15, 21, 7, 15, 22, 7, 15, 23, 8, 15, 24, 8, 15, 25, 8, 15, 26, 6, 16, 18, 6, 16, 19, 6, 16, 20, 7, 16, 21, 7, 16, 22, 7, 16, 23, 8, 16, 24, 8, 16, 25, 8, 16, 26, 6, 17, 18, 6, 17, 19, 6, 17, 20, 7, 17, 21, 7, 17, 22, 7, 17, 23, 8, 17, 24, 8, 17, 25, 8, 17, 26
 };
-
 int grppool[GROUPS_NUM];
 struct square {
   int ind, val, *gps[GROUPS_PER_SQUARE];
 } sqrpool[SQUARES_NUM];
 atomic_bool done;
-// ARGUMENTS
 struct config {
-  bool verbose, prgbar;
-  FILE *stream, *outfile, *infile;
   int maxsol, prgdep;
-  char *fnout;
+  FILE *outfile, *stream, *infile;
+  bool prgbar, verbose;
 };
 
 void aplval(int sqr) {
@@ -87,7 +84,7 @@ void initvals(int start) {
 }
 
 int rdfile(FILE *fp) {
-  int c, i, j, n, m;
+  int c, i, n, m;
 
   i = n = m = 0;
   while (i != SQUARES_NUM && (c = fgetc(fp)) != EOF) {
@@ -178,7 +175,7 @@ void prtprg(unsigned long long prg, int prgdep, FILE *stream) {
   fprintf(stream, "\e[91m]\e[0m\n");
 }
 
-void *function(void *arg) {
+void *barrtn(void *arg) {
   unsigned long long prg;
   struct config cfg = *((struct config *) arg);
 
@@ -186,8 +183,6 @@ void *function(void *arg) {
     prg = calcprg(cfg.prgdep);
     prtprg(prg, cfg.prgdep, cfg.stream);
   }
-
-  return NULL;
 }
 
 void prtbuf(const int *buf, FILE *stream) {
@@ -218,7 +213,15 @@ void prtbuf(const int *buf, FILE *stream) {
 }
 
 struct config prsargs(int argc, char **argv) {
-  struct config cfg = { false, true, stdout, NULL, NULL, 1, 3, NULL };
+  struct config cfg = {
+    .maxsol = false,
+    .outfile = NULL,
+    .prgbar = true,
+    .prgdep = 3,
+    .stream = stdout,
+    .verbose = false,
+    .infile = NULL
+  };
 
   while (argc) {
     if (!strcmp("--max-solutions", *argv) || !strcmp("-m", *argv)) {
@@ -268,7 +271,7 @@ struct config prsargs(int argc, char **argv) {
     } else if (!strcmp("--verbose", *argv) || !strcmp("-v", *argv)) {
       cfg.verbose = true;
     } else if (!strcmp("--help", *argv) || !strcmp("-h", *argv)) {
-      printf("\n\e[31mUsage:\e[91m sdksolv [\e[0moption...\e[91m] <\e[0mfile\e[91m>\n\n\e[31mOptions:\e[0m\n  -\e[91mm\e[0m, --\e[91mmax-solutions <\e[0mnumber\e[91m>\e[0m          Stop after n solutions have been found\n  -\e[91mo\e[0m, --\e[91moutput <\e[0mfile\e[91m>\e[0m                   Print solutions in file\n\n  -\e[91mb\e[0m, --\e[91mhide-progress-bar\e[0m               Disable progress bar\n  -\e[91md\e[0m, --\e[91mprogress-update-depth <\e[0mnumber\e[91m>\e[0m  Precision of progress bar\n\n  -\e[91mq\e[0m, --\e[91mquiet\e[0m                           Only print errors\n  -\e[91mv\e[0m, --\e[91mverbose\e[0m                         Print out all found solutions\n\n  -\e[91mh\e[0m, --\e[91mhelp\e[0m                            Print this page\n\n");
+      printf("\n\e[31mUsage:\e[91m sdksolv [\e[0moption...\e[91m] <\e[0mfile\e[91m>\n\n\e[31mOptions:\e[0m\n  -\e[91mm\e[0m, --\e[91mmax-solutions <\e[0mnumber\e[91m>\e[0m          Stop after n solutions have been found\n  -\e[91mo\e[0m, --\e[91moutput <\e[0mfile\e[91m>\e[0m                   Print solutions to file\n\n  -\e[91mb\e[0m, --\e[91mhide-progress-bar\e[0m               Disable progress bar\n  -\e[91md\e[0m, --\e[91mprogress-update-depth <\e[0mnumber\e[91m>\e[0m  Precision of progress bar\n\n  -\e[91mq\e[0m, --\e[91mquiet\e[0m                           Only print errors\n  -\e[91mv\e[0m, --\e[91mverbose\e[0m                         Print out all found solutions\n\n  -\e[91mh\e[0m, --\e[91mhelp\e[0m                            Print this page\n\n");
       exit(EXIT_SUCCESS);
     } else if(**argv == '-') {
       fprintf(stderr, "\n\e[31mError:\e[91m Unknown option\e[0m %s\e[91m!\e[0m\n\n\e[32mHint:\e[92m Use\e[0m sdksolv --help \e[92mfor help page.\e[0m\n\n", *argv);
@@ -284,6 +287,7 @@ struct config prsargs(int argc, char **argv) {
         exit(EXIT_FAILURE);
       }
     }
+
     ++argv;
     --argc;
   }
@@ -292,6 +296,7 @@ struct config prsargs(int argc, char **argv) {
     fprintf(stderr, "\n\e[31mError:\e[91m No input file specified!\e[0m\n\n", *argv);
     exit(EXIT_FAILURE);
   }
+
   if(cfg.prgdep > 9)
     fprintf(cfg.stream, "\n\e[33mWarning:\e[93m High progress-update-depth may result in graphical errors!\e[0m\n\n");
 
@@ -300,11 +305,8 @@ struct config prsargs(int argc, char **argv) {
 
 int main(int argc, char **argv) {
   struct config cfg = prsargs(argc - 1, argv + 1);
-
-  int end = rdfile(cfg.infile);
-  int buf[cfg.maxsol * SQUARES_NUM];
-  int s, i;
-  pthread_t prgbar;
+  int s, i, end = rdfile(cfg.infile), buf[cfg.maxsol * SQUARES_NUM];
+  pthread_t barthrd;
 
   fprintf(cfg.stream, "\n\e[91m          < SUDOKU SOLVER >          \e[31m\n\n  Read-in field:\e[0m\n");
   wrtobuf(buf);
@@ -313,7 +315,7 @@ int main(int argc, char **argv) {
   fprintf(cfg.stream, "\n\n");
   if (cfg.prgbar) {
     done = false;
-    pthread_create(&prgbar, NULL, function, &cfg);
+    pthread_create(&barthrd, NULL, &barrtn, &cfg);
   }
 
   initgps();
@@ -321,7 +323,7 @@ int main(int argc, char **argv) {
   s = solve(end, cfg.maxsol, buf);
   if (cfg.prgbar) {
     done = true;
-    pthread_join(prgbar, NULL);
+    pthread_join(barthrd, NULL);
     prtprg(pow(GROUP_SIZE, cfg.prgdep), cfg.prgdep, cfg.stream);
   }
 
