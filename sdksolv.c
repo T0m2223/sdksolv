5500ee44d7135dbd2224828325e0c9b462ec919f
T0m2223 <64265856+T0m2223@users.noreply.github.com>
2022-04-15 13:37:42 +0200

    Update sdksolv.c

--- sdksolv.c
+++ sdksolv.c
@@ -256,8 +256,8 @@ struct config prsargs(int argc, char **argv) {
         exit(EXIT_FAILURE);
       }
       cfg.prgdep = atoi(argv[1]);
-      if (cfg.prgdep < 1) {
-        fprintf(stderr, "\n\x1b[31mError:\x1b[91m Progress-update-depth must be a numeric value and at least\x1b[0m 1\x1b[91m!\x1b[0m\n\n");
+      if (cfg.prgdep < 1 || cfg.prgdep >= SQUARES_NUM) {
+        fprintf(stderr, "\n\x1b[31mError:\x1b[91m Progress-update-depth must be a numeric value and in range\x1b[0m 1 - 81\x1b[91m!\x1b[0m\n\n");
         exit(EXIT_FAILURE);
       }
       ++argv;
